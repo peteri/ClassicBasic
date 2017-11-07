d230ba56259aa60bfb0e7473d50c5ad0a704b8dd
Peter Ibbotson <peter.ibbotson@trayport.com>
2017-11-07 18:41:43 +0000

    Fix get so tests now work.

--- ClassicBasic.Interpreter/Commands/Get.cs
+++ ClassicBasic.Interpreter/Commands/Get.cs
@@ -4,46 +4,65 @@
 
 namespace ClassicBasic.Interpreter.Commands
 {
-    /// <summary>
-    /// Implements the GET command.
-    /// </summary>
-    public class Get : Token, ICommand
-    {
-        private readonly IRunEnvironment _runEnvironment;
-        private readonly IExpressionEvaluator _expressionEvaluator;
-        private readonly ITeletypeWithPosition _teletypeWithPosition;
+   /// <summary>
+   /// Implements the GET command.
+   /// </summary>
+   public class Get : Token, ICommand
+   {
+      private readonly IRunEnvironment _runEnvironment;
+      private readonly IExpressionEvaluator _expressionEvaluator;
+      private readonly ITeletypeWithPosition _teletypeWithPosition;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Get"/> class.
-        /// </summary>
-        /// <param name="runEnvironment">Run time environment.</param>
-        /// <param name="expressionEvaluator">Expression evaluator.</param>
-        /// <param name="teletypeWithPosition">Teletype.</param>
-        public Get(
-            IRunEnvironment runEnvironment,
-            IExpressionEvaluator expressionEvaluator,
-            ITeletypeWithPosition teletypeWithPosition)
-            : base("GET", TokenType.ClassStatement)
-        {
-            _runEnvironment = runEnvironment;
-            _expressionEvaluator = expressionEvaluator;
-            _teletypeWithPosition = teletypeWithPosition;
-        }
+      /// <summary>
+      /// Initializes a new instance of the <see cref="Get"/> class.
+      /// </summary>
+      /// <param name="runEnvironment">Run time environment.</param>
+      /// <param name="expressionEvaluator">Expression evaluator.</param>
+      /// <param name="teletypeWithPosition">Teletype.</param>
+      public Get(
+          IRunEnvironment runEnvironment,
+          IExpressionEvaluator expressionEvaluator,
+          ITeletypeWithPosition teletypeWithPosition)
+          : base("GET", TokenType.ClassStatement)
+      {
+         _runEnvironment = runEnvironment;
+         _expressionEvaluator = expressionEvaluator;
+         _teletypeWithPosition = teletypeWithPosition;
+      }
 
-        /// <summary>
-        /// Executes the GET command.
-        /// </summary>
-        public void Execute()
-        {
-            if (!_runEnvironment.CurrentLine.LineNumber.HasValue)
+      /// <summary>
+      /// Executes the GET command.
+      /// </summary>
+      public void Execute()
+      {
+         if (!_runEnvironment.CurrentLine.LineNumber.HasValue)
+         {
+            throw new Exceptions.IllegalDirectException();
+         }
+
+         var variableReference = _expressionEvaluator.GetLeftValue();
+         var newChar = _teletypeWithPosition.ReadChar();
+         Accumulator newValue;
+         if (variableReference.GetValue().Type == typeof(string))
+         {
+            newValue = new Accumulator(newChar.ToString());
+         }
+         else
+         {
+            if ("+-E.\0".Contains(newChar.ToString()))
             {
-                throw new Exceptions.IllegalDirectException();
+               newChar = '0';
             }
 
-            var variableReference = _expressionEvaluator.GetLeftValue();
-            var newChar = _teletypeWithPosition.ReadChar();
-            Accumulator newValue = new Accumulator(newChar.ToString());
-            variableReference.SetValue(newValue);
-        }
-    }
+            if (newChar < '0' || newChar > '9')
+            {
+               throw new Exceptions.SyntaxErrorException();
+            }
+
+            newValue = new Accumulator((double)(newChar - '0'));
+         }
+
+         variableReference.SetValue(newValue);
+      }
+   }
 }
