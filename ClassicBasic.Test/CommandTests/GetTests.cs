eece77f360d44626e0e18963735e016bde2f2892
Peter Ibbotson <peter@ibbotson.co.uk>
2017-11-07 01:31:40 +0000

    Added failing tests for get

--- ClassicBasic.Test/CommandTests/GetTests.cs
+++ ClassicBasic.Test/CommandTests/GetTests.cs
@@ -4,7 +4,11 @@
 
 namespace ClassicBasic.Test.CommandTests
 {
+    using System.Collections.Generic;
+    using ClassicBasic.Interpreter;
+    using ClassicBasic.Interpreter.Commands;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
 
     /// <summary>
     /// Tests the GET statement.
@@ -12,13 +16,105 @@ namespace ClassicBasic.Test.CommandTests
     [TestClass]
     public class GetTests
     {
+        private IVariableRepository _variableRepository;
+        private Mock<IExpressionEvaluator> _mockExpressionEvaluator;
+        private Mock<ITeletypeWithPosition> _mockTeletypeWithPosition;
+        private IRunEnvironment _runEnvironment;
+        private Get _sut;
+
         /// <summary>
-        /// Stub.
+        /// Tests Get for string variable.
         /// </summary>
         [TestMethod]
-        public void GetDoesSomething()
+        public void GetWorksWithStringVariable()
         {
-            Assert.Inconclusive();
+            SetupSut();
+            _mockTeletypeWithPosition.Setup(mtwp => mtwp.ReadChar()).Returns('1');
+            var variableReference = _variableRepository.GetOrCreateVariable("A$", new short[] { });
+            _mockExpressionEvaluator.Setup(mee => mee.GetLeftValue()).Returns(variableReference);
+            _sut.Execute();
+            Assert.AreEqual("1", variableReference.GetValue().ValueAsString());
+        }
+
+        /// <summary>
+        /// Tests Get throws for immediate mode.
+        /// </summary>
+        [TestMethod]
+        public void GetThrowsForImmediate()
+        {
+            SetupSut();
+            _mockTeletypeWithPosition.Setup(mtwp => mtwp.ReadChar()).Returns('1');
+            var variableReference = _variableRepository.GetOrCreateVariable("A$", new short[] { });
+            _mockExpressionEvaluator.Setup(mee => mee.GetLeftValue()).Returns(variableReference);
+            _runEnvironment.CurrentLine = new ProgramLine(null, new List<IToken> { });
+            bool exceptionThrown = false;
+            try
+            {
+                _sut.Execute();
+            }
+            catch (ClassicBasic.Interpreter.Exceptions.IllegalDirectException)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsTrue(exceptionThrown);
+        }
+
+        /// <summary>
+        /// Tests Get throws syntax error for Alpha in numeric mode.
+        /// </summary>
+        [TestMethod]
+        public void GetThrowsForAlphaInNumeric()
+        {
+            SetupSut();
+            _mockTeletypeWithPosition.Setup(mtwp => mtwp.ReadChar()).Returns('A');
+            var variableReference = _variableRepository.GetOrCreateVariable("A", new short[] { });
+            _mockExpressionEvaluator.Setup(mee => mee.GetLeftValue()).Returns(variableReference);
+            bool exceptionThrown = false;
+            try
+            {
+                _sut.Execute();
+            }
+            catch (ClassicBasic.Interpreter.Exceptions.SyntaxErrorException)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsTrue(exceptionThrown);
+        }
+
+        /// <summary>
+        /// Tests Get for numeric variable.
+        /// The plus sign, minus sign, ctrl @, E, space and the period all return a zero as the typed value.
+        /// </summary>
+        /// <param name="character">Character from keyboard.</param>
+        /// <param name="expectedValue">Expected value.</param>
+        [DataTestMethod]
+        [DataRow('+', 0.0)]
+        [DataRow('-', 0.0)]
+        [DataRow('E', 0.0)]
+        [DataRow('.', 0.0)]
+        [DataRow('1', 1.0)]
+        [DataRow('9', 9.0)]
+        [DataRow((char)0, 0.0)]
+        public void GetWorksWithNumericVariable(char character, double expectedValue)
+        {
+            SetupSut();
+            _mockTeletypeWithPosition.Setup(mtwp => mtwp.ReadChar()).Returns(character);
+            var variableReference = _variableRepository.GetOrCreateVariable("A", new short[] { });
+            _mockExpressionEvaluator.Setup(mee => mee.GetLeftValue()).Returns(variableReference);
+            _sut.Execute();
+            Assert.AreEqual(expectedValue, variableReference.GetValue().ValueAsDouble());
+        }
+
+        private void SetupSut()
+        {
+            _runEnvironment = new RunEnvironment();
+            _variableRepository = new VariableRepository();
+            _mockExpressionEvaluator = new Mock<IExpressionEvaluator>();
+            _mockTeletypeWithPosition = new Mock<ITeletypeWithPosition>();
+            _runEnvironment.CurrentLine = new ProgramLine(10, new List<IToken> { });
+            _sut = new Get(_runEnvironment, _mockExpressionEvaluator.Object, _mockTeletypeWithPosition.Object);
         }
     }
 }
